24372ff6fa57768eb3d7a9f9a0b5e80c6981c8dc
Tony-S201 <swierz.tony@gmail.com>
2024-11-06 19:29:49 +0100

    core-front: prevent CORS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL + '/api/:path*' || 'http://127.0.0.1:5000/api/:path*'
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
